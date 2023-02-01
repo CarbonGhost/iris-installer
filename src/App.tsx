d460025b794ddec35b193e7b68fbd44c51325596
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2023-02-01 13:43:25 +0800

    Initial Front End
    Add the initial implementation of the front end UI. Unpolished and unfinished.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,105 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { createSignal, useTransition } from "solid-js"
 
-function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+export default function App() {
+	const themes: { bg: string; color: string }[] = [
+		{
+			bg: "birch",
+			color: "#8ca641"
+		},
+		{
+			bg: "geode",
+			color: "#793cb9"
+		},
+		{
+			bg: "village",
+			color: "#a3ba3f"
+		}
+	]
+	const [theme, setTheme] = createSignal(themes[0])
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
+	let i = 0
+	setInterval(() => {
+		i++
+		if (i == themes.length) {
+			i = 0
+		}
+		setTheme(themes[i])
+	}, 5000)
 
-  return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-      <div class="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+	return (
+		<>
+			{/* Background image container */}
+			<div
+				class="content-between h-screen text-sm duration-1000 bg-no-repeat bg-cover"
+				style={{
+					"background-image": `url('../src/assets/backgrounds/${
+						theme().bg
+					}.png')`
+				}}>
+				<div
+					data-tauri-drag-region
+					class="mix-blend-overlay bg-white/20 hover:bg-white/40 fixed z-50 w-full h-6 duration-500"
+				/>
+				{/* Main content container */}
+				<div class="backdrop-blur-sm bg-gradient-to-t from-zinc-800 via-zinc-800/70 to-transparent grid h-full grid-cols-1 px-12 py-16">
+					{/* Text content container */}
+					<div class="self-center space-y-2">
+						<header class="text-5xl font-bold text-center text-gray-100">
+							<img
+								class="inline w-12 h-12 mr-2 align-bottom"
+								src="../app-icon.png"
+							/>
+							Install Iris
+						</header>
+						<p class="text-center text-gray-200">
+							Welcome to the Iris Installer, this program will get
+							you up and running with next-gen Minecraft shaders
+							in minutes. Simply click "Install" to use our
+							sensible defaults, or customize your installation if
+							you are a power user.
+						</p>
+					</div>
+					<div class="gap-y-2 flex flex-col self-center text-center text-gray-200">
+						<p>Advanced install option</p>
+						<p>Advanced install option</p>
+						<p>Advanced install option</p>
+						<p>Advanced install option</p>
+					</div>
+					<div class="self-end space-y-2">
+						<button class="hover:brightness-125 border-zinc-500 text-zinc-400 w-full py-1.5 font-semibold duration-500 border-2 rounded">
+							Iris for 1.19.3
+						</button>
+						<button
+							class="hover:brightness-125 text-zinc-800 w-full py-1.5 font-semibold duration-500 rounded"
+							style={{ "background-color": theme().color }}>
+							Install
+						</button>
+						<div class="flex justify-center gap-6">
+							<a
+								class="hover:underline hover:brightness-125 duration-500"
+								href="https://discord.gg/the-iris-project-774352792659820594"
+								target="_blank"
+								style={{ color: theme().color }}>
+								Get Support
+							</a>
+							<a
+								class="hover:underline hover:brightness-125 duration-500"
+								href="https://irisshaders.net/"
+								target="_blank"
+								style={{ color: theme().color }}>
+								Website
+							</a>
+							<a
+								class="hover:underline hover:brightness-125 duration-500"
+								href="https://github.com/IrisShaders"
+								target="_blank"
+								style={{ color: theme().color }}>
+								GitHub
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
-
-export default App;
